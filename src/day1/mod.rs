18c431f670ce791d061bb82443a7374a609e19ed
Jorik <vanveenjorik@protonmail.com>
2025-12-02 00:33:42 +0100

    Day 1 p1,2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,3 +1,5 @@
+use std::ops::Rem;
+
 use crate::{DayImpl, InputType};
 
 pub struct Day {
@@ -20,12 +22,80 @@ impl Day {
     }
 }
 
+#[derive(Debug, Clone)]
+struct Rotation {
+    direction: Direction,
+    degrees: i16,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Direction {
+    Left,
+    Right,
+}
+
+fn parse_input(input: &str) -> Vec<Rotation> {
+    input
+        .lines()
+        .map(|line| {
+            let (dir_char, deg_str) = line.split_at(1);
+            let direction = match dir_char {
+                "L" => Direction::Left,
+                "R" => Direction::Right,
+                _ => panic!("Invalid direction character: {}", dir_char),
+            };
+            let degrees: i16 = deg_str.parse().expect("Invalid degrees");
+            Rotation { direction, degrees }
+        })
+        .collect()
+}
+
 impl DayImpl for Day {
     fn part_one(&self) -> String {
-        todo!()
+        let input = parse_input(self.get_input());
+
+        let mut pointing_at = 50;
+        let mut zero_count = 0;
+
+        for Rotation { degrees, direction } in input {
+            pointing_at = match direction {
+                Direction::Left => pointing_at - degrees,
+                Direction::Right => pointing_at + degrees,
+            } % 100;
+
+            if pointing_at == 0 {
+                zero_count += 1;
+            }
+        }
+
+        format!("{}", zero_count)
     }
 
     fn part_two(&self) -> String {
-        todo!()
+        let input = parse_input(self.get_input());
+        let mut pointing_at = 50;
+        let mut zero_count = 0;
+
+        for Rotation { degrees, direction } in input {
+            let full_rotations = degrees / 100;
+            zero_count += full_rotations;
+
+            let degrees = degrees.rem_euclid(100);
+            let old_pointing_at = pointing_at.clone();
+            pointing_at = match direction {
+                Direction::Left => pointing_at - degrees,
+                Direction::Right => pointing_at + degrees,
+            };
+            if (pointing_at < 0 && old_pointing_at != 0) || pointing_at > 100 {
+                zero_count += 1;
+            }
+            pointing_at = pointing_at.rem_euclid(100);
+
+            if pointing_at == 0 {
+                zero_count += 1;
+            }
+        }
+
+        format!("{}", zero_count)
     }
 }
